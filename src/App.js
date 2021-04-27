cfb0ac8a07b153039808e47e89f702c4d188a8ef
marco corapi <marco.corapi@outlook.it>
2021-04-27 18:01:19 +0200

    axios

--- src/App.js
+++ src/App.js
@@ -14,6 +14,18 @@ export default App;
 
 /* 
   1. take an api: https://www.weatherapi.com/
-  2. copy your API key, put it in the git ignore file and use it to 
-  take data from the API explorer
+  2. copy your API key, and use it to take data from the API explorer. 
+     we get some data back and a call, a link which contains our API key
+     and the q=query :
+     http://api.weatherapi.com/v1/current.json?key=00000000000000&q=copenhagen&aqi=no
+     a query provides a search and suggest methods for building results in an application.
+     in our case is the city.
+  
+  3. npm install axios
+  
+  4. import axios from 'axios';
+     Axios is a library that helps us make http requests to external resources. 
+     In our React applications we often need to retrieve data from external APIs so it can 
+     be displayed in our web pages
+
 */
