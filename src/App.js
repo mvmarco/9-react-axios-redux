351a0e345ca7faa6f92beb1f1a9a56065683c81d
marco corapi <marco.corapi@outlook.it>
2021-04-27 17:51:58 +0200

    some note abt API

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Axios and Redux</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// ############################
+// AXIOS AND APIs
+// ############################
+
+/* 
+  1. take an api: https://www.weatherapi.com/
+  2. copy your API key, put it in the git ignore file and use it to 
+  take data from the API explorer
+*/
