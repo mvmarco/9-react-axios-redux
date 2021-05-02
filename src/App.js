e539314230d5d96dd40546f386dce14f5acaa8ac
marco corapi <marco.corapi@outlook.it>
2021-05-02 13:41:04 +0200

    draft

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default App;
      of the API, which we will then log to check them out:
 
       const location = axios.get(
-        'http://api.weatherapi.com/v1/current.json?key=fb454c4007ae4b63b83154929212704&q=copenhagen&aqi=no'
+        'http://api.weatherapi.com/v1/current.json?key=XXXXXXXXX&q=copenhagen&aqi=no'
       ).then((data) => {
         console.log(data)
       }).catch(err => console.log(err, "Sorry something went wrong, try later"));
@@ -44,9 +44,26 @@ export default App;
       .then is like saying API.data to get the value, but since it is a promise you cannot do it
       in this the way. .catch is in case the promise does not work.
 
-      check the console:) data.location will give us the location and so on
-      like current.something will give us the
+      data is the whole data that we have, the API has a key called data where there are the info we
+      need so we do data.data (example)
 
+      check the console:) data.data.location will give us the location and so on
+      like data.date.current.something will give us the
+
+
+
+// ############################
+// .ENV
+// ############################
+
+  When working with API, few steps are necessary:
+
+  1. In the root folder of your project create a: .env file
+  2. write: REACT_APP followed by the name of your project or whatever name you want to give
+     to your api key. such as: REACT_APP_WEATHER_APP= XXXXXXXXXXXXXXXX here you paste the api key
+     from your app and replace it with:
+     from: 'http://api.weatherapi.com/v1/current.json?key=XXXXXXXXX&q=copenhagen&aqi=no'
+     to (backticks also): `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=copenhagen&aqi=no`
 
 
 */
